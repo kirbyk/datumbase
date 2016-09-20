ccb7ea9d060e767c7ef0e2a777891cfa33c1f4d9
ᴋɪʀʙʏ ᴋᴏʜʟᴍᴏʀɢᴇɴ <kirby@kkohlmorgen.com>
2016-09-19 21:44:24 -0400

    Add first example

--- examples/website.js
+++ examples/website.js
@@ -1,3 +1,12 @@
-var datumbase = require('../datumbase.js');
+var Datumbase = require('../datumbase.js');
+var http = require('http');
 
-datumbase();
+var name = new Datumbase('Kirby Kohlmorgen');
+
+var server = http.createServer(function(req, res) {
+  res.end(name.get());
+});
+
+server.listen(8080, function() {
+  console.log('Server listening on port 8080.');
+});
