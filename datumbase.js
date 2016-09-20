3c7434760accef7b36ce37aa981e98a1a32a5976
ᴋɪʀʙʏ ᴋᴏʜʟᴍᴏʀɢᴇɴ <kirby@kkohlmorgen.com>
2016-09-19 21:44:18 -0400

    Add first datumbase implementation

--- datumbase.js
+++ datumbase.js
@@ -1,3 +1,21 @@
-module.exports = function() {
-  console.log('Hello world');
+'use strict';
+
+function Datumbase(datum) {
+  this.datum = datum;
 };
+
+Datumbase.prototype.set = function(datum) {
+  if (!this.datum && this.datum !== false && this.datum !== NaN) {
+    throw "Can't set nothing!";
+  }
+  this.datum = datum;
+};
+
+Datumbase.prototype.get = function() {
+  if (!this.datum && this.datum !== false && this.datum !== NaN) {
+    throw "Can't get nothing!";
+  }
+  return this.datum;
+}
+
+module.exports = Datumbase;
